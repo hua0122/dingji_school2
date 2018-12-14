33cc1e1850efdd02d8b0071ca341d99bed81ffc6
VIPsth <VIPsongtaohong@yeah.net>
2018-12-14 16:53:25 +0800

    add

--- enlist/api.js
+++ enlist/api.js
@@ -54,6 +54,7 @@ function get_area() {
 			wx.getLocation({
 				type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
 				success: function(res) {
+					alert("嘿嘿")
 					alert(res.longitude+", "+res.latitude)
 					area(res.longitude, res.latitude);
 				},
