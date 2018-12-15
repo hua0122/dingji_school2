7eb28ee065351b11131dd0beb371cb85d652447b
VIPsth <VIPsongtaohong@yeah.net>
2018-12-15 15:30:35 +0800

    add

--- static/js/custom1.js
+++ static/js/custom1.js
@@ -64,7 +64,7 @@ function getydxc() {
 				});
 				var title = "秀学车服务平台";
 				// var link = "http://ydxctrue.yidianxueche.cn/";
-				var link = "http://xxc.yidianxueche.cn/";
+				var link = window.location.href;
 				var imgUrl = "http://ydxctrue.yidianxueche.cn/template/wap/public/css/self/image/banner_1.jpg";
 				var desc = "人工智能引领驾培未来，秀学车定制中国好司机";
 				var type = "";
