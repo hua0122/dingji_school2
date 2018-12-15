e88f612f879964e9bce3c4113b816b84ea2ba417
VIPsth <VIPsongtaohong@yeah.net>
2018-12-15 15:45:48 +0800

    add

--- static/js/custom1.js
+++ static/js/custom1.js
@@ -54,7 +54,7 @@ function getydxc() {
 				wx.config({
 					debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 					appId: data.content.appId, // 必填，公众号的唯一标识
-					timestamp: parseInt(data.content.timestamp), // 必填，生成签名的时间戳
+					timestamp: data.content.timestamp, // 必填，生成签名的时间戳
 					nonceStr: data.content.nonceStr, // 必填，生成签名的随机串
 					signature: data.content.signature, // 必填，签名，见附录1
 					jsApiList: ['updateTimelineShareData','updateAppMessageShareData', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ',
@@ -62,100 +62,100 @@ function getydxc() {
 						'onMenuShareQZone', 'getLocation', 'chooseWXPay'
 					] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 				});
+
 				var title = "秀学车服务平台";
 				// var link = "http://ydxctrue.yidianxueche.cn/";
-				var link = window.location.href;
-				console.log(link)
+				var link = "http://xxc.yidianxueche.cn/";
 				var imgUrl = "http://ydxctrue.yidianxueche.cn/template/wap/public/css/self/image/banner_1.jpg";
 				var desc = "人工智能引领驾培未来，秀学车定制中国好司机";
 				var type = "";
 				var dataUrl = "";
-// 				wx.ready(function() {
-// 					wx.getLocation({
-// 						type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-// 						success: function(res) {
-// 							var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-// 							var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-// 							sessionStorage.setItem("latlng", JSON.stringify({
-// 								lat: latitude,
-// 								lng: longitude
-// 							}))
-// 						}
-// 					});
-// 					wx.updateTimelineShareData({
-// 						title: title, // 分享标题
-// 						desc: desc, // 分享描述
-// 						link: link, // 分享链接
-// 						imgUrl: imgUrl, // 分享图标
-// 						trigger: function(res) {},
-// 						success: function(res) {
-// 							// document.title="新接口已分享"
-// 						},
-// 						cancel: function(res) {},
-// 						fail: function(res) {}
-// 					});
-// 					wx.onMenuShareTimeline({
-// 						title: title, // 分享标题
-// 						link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-// 						imgUrl: imgUrl, // 分享图标
-// 						success: function() {
-// 							// 用户确认分享后执行的回调函数
-// 						},
-// 						cancel: function() {
-// 							// 用户取消分享后执行的回调函数
-// 						}
-// 					});
-// 					wx.onMenuShareAppMessage({
-// 						title: title, // 分享标题
-// 						desc: desc, // 分享描述
-// 						link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-// 						imgUrl: imgUrl, // 分享图标
-// 						type: type, // 分享类型,music、video或link，不填默认为link
-// 						dataUrl: dataUrl, // 如果type是music或video，则要提供数据链接，默认为空
-// 						success: function() {
-// 							// 用户确认分享后执行的回调函数
-// 						},
-// 						cancel: function() {
-// 							// 用户取消分享后执行的回调函数
-// 						}
-// 					});
-// 					wx.onMenuShareQQ({
-// 						title: title, // 分享标题
-// 						desc: desc, // 分享描述
-// 						link: link, // 分享链接
-// 						imgUrl: imgUrl, // 分享图标
-// 						success: function() {
-// 							// 用户确认分享后执行的回调函数
-// 						},
-// 						cancel: function() {
-// 							// 用户取消分享后执行的回调函数
-// 						}
-// 					});
-// 					wx.onMenuShareWeibo({
-// 						title: title, // 分享标题
-// 						desc: desc, // 分享描述
-// 						link: link, // 分享链接
-// 						imgUrl: imgUrl, // 分享图标
-// 						success: function() {
-// 							// 用户确认分享后执行的回调函数
-// 						},
-// 						cancel: function() {
-// 							// 用户取消分享后执行的回调函数
-// 						}
-// 					});
-// 					wx.onMenuShareQZone({
-// 						title: title, // 分享标题
-// 						desc: desc, // 分享描述
-// 						link: link, // 分享链接
-// 						imgUrl: imgUrl, // 分享图标
-// 						success: function() {
-// 							// 用户确认分享后执行的回调函数
-// 						},
-// 						cancel: function() {
-// 							// 用户取消分享后执行的回调函数
-// 						}
-// 					});
-// 				});
+				wx.ready(function() {
+					wx.getLocation({
+						type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+						success: function(res) {
+							var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+							var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+							sessionStorage.setItem("latlng", JSON.stringify({
+								lat: latitude,
+								lng: longitude
+							}))
+						}
+					});
+					wx.updateTimelineShareData({
+						title: title, // 分享标题
+						desc: desc, // 分享描述
+						link: link, // 分享链接
+						imgUrl: imgUrl, // 分享图标
+						trigger: function(res) {},
+						success: function(res) {
+							// document.title="新接口已分享"
+						},
+						cancel: function(res) {},
+						fail: function(res) {}
+					});
+					wx.onMenuShareTimeline({
+						title: title, // 分享标题
+						link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+						imgUrl: imgUrl, // 分享图标
+						success: function() {
+							// 用户确认分享后执行的回调函数
+						},
+						cancel: function() {
+							// 用户取消分享后执行的回调函数
+						}
+					});
+					wx.onMenuShareAppMessage({
+						title: title, // 分享标题
+						desc: desc, // 分享描述
+						link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+						imgUrl: imgUrl, // 分享图标
+						type: type, // 分享类型,music、video或link，不填默认为link
+						dataUrl: dataUrl, // 如果type是music或video，则要提供数据链接，默认为空
+						success: function() {
+							// 用户确认分享后执行的回调函数
+						},
+						cancel: function() {
+							// 用户取消分享后执行的回调函数
+						}
+					});
+					wx.onMenuShareQQ({
+						title: title, // 分享标题
+						desc: desc, // 分享描述
+						link: link, // 分享链接
+						imgUrl: imgUrl, // 分享图标
+						success: function() {
+							// 用户确认分享后执行的回调函数
+						},
+						cancel: function() {
+							// 用户取消分享后执行的回调函数
+						}
+					});
+					wx.onMenuShareWeibo({
+						title: title, // 分享标题
+						desc: desc, // 分享描述
+						link: link, // 分享链接
+						imgUrl: imgUrl, // 分享图标
+						success: function() {
+							// 用户确认分享后执行的回调函数
+						},
+						cancel: function() {
+							// 用户取消分享后执行的回调函数
+						}
+					});
+					wx.onMenuShareQZone({
+						title: title, // 分享标题
+						desc: desc, // 分享描述
+						link: link, // 分享链接
+						imgUrl: imgUrl, // 分享图标
+						success: function() {
+							// 用户确认分享后执行的回调函数
+						},
+						cancel: function() {
+							// 用户取消分享后执行的回调函数
+						}
+					});
+				});
 			} else {
 				//alert('系统繁忙！稍后再试！');
 			}
