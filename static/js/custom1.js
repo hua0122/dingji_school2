d65343732889d1bee35b1a4ca5b0e8346578007e
VIPsth <VIPsongtaohong@yeah.net>
2018-12-15 15:21:30 +0800

    add

--- static/js/custom1.js
+++ static/js/custom1.js
@@ -55,7 +55,7 @@ function getydxc() {
 					debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 					appId: data.content.appId, // 必填，公众号的唯一标识
 					timestamp: data.content.timestamp, // 必填，生成签名的时间戳
-					noncestr: data.content.nonceStr, // 必填，生成签名的随机串
+					nonceStr: data.content.nonceStr, // 必填，生成签名的随机串
 					signature: data.content.signature, // 必填，签名，见附录1
 					jsApiList: ['updateTimelineShareData','updateAppMessageShareData', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ',
 						'onMenuShareWeibo',
